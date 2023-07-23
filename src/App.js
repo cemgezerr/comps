55f76a4b788dfa4b4ab92e322c7831e2c03cad80
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-23 16:50:17 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div><Button primary rounded outline>Selamlar</Button></div>
+      <div><Button secondary>Satın Al</Button></div>
+      <div><Button success >İade Et</Button></div>
+      <div><Button warning >Kargola</Button></div>
+      <div><Button danger>Ödeme Yap</Button></div>
+    </div>
+    
   )
 }
 
---
+++ src/Button.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import classNames from 'classnames'
+
+const Button = ({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) => {
+    const classes = classNames('flex items-center px-3 py-1.5  border', {
+        'bg-blue-500  border-blue-500 text-white':primary,
+        'bg-gray-900  border-gray-900 text-white':secondary,
+        'bg-green-500  border-green-500 text-white':success,
+        'bg-yellow-400  border-yellow-400 text-white':warning,
+        'bg-red-500  border-red-500 text-white':danger,
+        'rounded-full' : rounded,
+        'bg-white':outline,
+        'text-blue-600': outline && primary,
+        'text-gray-500': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-500': outline && warning,
+        'text-red-500': outline && danger,
+    })
+
+    return (
+        <button className={classes}>{children}</button>
+    )
+}
+Button.propTypes = {
+    checkVariationValue: ({ primary, success, secondary, warning, danger }) => {
+        const count = Number(!!primary) + Number(!!secondary) + Number(!!warning) + Number(!!success) + Number(!!danger)
+        if (count > 1) {
+            return new Error('tek bir tane prop giriniz')
+        }
+    }
+}
+
+export default Button
