e5d755972660d236fa472f4cbeff42f42dfb4799
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-24 16:01:28 +0300

    navigation pages add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from './components/Link'
+import Route from './components/Route'
+import AccordionPage from './pages/AccordionPage'
+import DropDownPage from './pages/DropdownPage'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Link to={"/accordion"}>go accordion</Link>
+      <Link to={"/dropdown"}>go dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropDownPage />
+        </Route>
+      </div>
+    </div>
   )
 }
 
