cb75f196630108ff0f5223e3658d27b497b08dd9
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-24 10:02:31 +0300

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import DropDown from './components/DropDown'
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [selection,setSelection] = useState(null);
+
+  const handleSelection = (option) => {
+    setSelection(option);
+  }
   const option = [
-    {label:'Red', value:'red'},
+     {label:'Red', value:'red'},
     {label:'Green', value:'green'},
     {label:'Blue', value:'blue'}
 
   ]
   return (
-    <div><DropDown option={option}/></div>
+    <div><DropDown option={option} selection={selection} onSelect={handleSelection}/></div>
   )
 }
 
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
-const DropDown = () => {
+const DropDown = ({ option, selection, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+  const handleOptionClick = (option) => {
+    setIsOpen(false)
+    onSelect(option)
+  }
+
+
+  const renderedOption = option.map((option) => {
+    return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+  })
+
+  let content = 'Select...'
+  if (selection) {
+    content = selection.label
+  }
   return (
-    <div>DropDown</div>
+    <div>
+      <div onClick={handleClick}>{content}</div>
+      <div>
+        {isOpen && <div>{renderedOption}</div>}
+      </div>
+    </div>
   )
 }
 
