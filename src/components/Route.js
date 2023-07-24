f070064943b6fac7dad57fe79a948f04cf0ec254
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-24 16:00:22 +0300

    route add

---
+++ src/components/Route.js
@@ -0,0 +1,12 @@
+import useNavigation from "../hooks/use-navigation";
+
+const Route = ({path,children}) => {
+    const {currentPath} = useNavigation();
+
+    if(path === currentPath){
+        return children;
+    }
+  return null;
+}
+
+export default Route;
