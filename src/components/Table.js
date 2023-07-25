304fab69c222c13b7595fa0436133b784ea4f625
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-25 13:35:16 +0300

    table first setup

---
+++ src/components/Table.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Table = ({ data , config}) => {
+    const renderedHeader = config.map((column) => {
+      return <th key={column.label}>{column.label}</th>
+    })
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr className='border-b' key={fruit.name}>
+                <td className='p-3'>{fruit.name}</td>
+                <td className='p-3'>
+                    <div className={`p-3 m-2 ${fruit.color}`}>
+                    </div></td>
+                <td className='p-3'>{fruit.score}</td>
+            </tr>
+        )
+    })
+    return (
+        <table className='table-auto border-spacing-2'>
+            <thead>
+                <tr className='border-b-2'>
+                 {renderedHeader}
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
