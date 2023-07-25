6847ba419607f46ed1778558a9a0ea27bff19f91
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-25 13:35:41 +0300

    tablepage add

---
+++ src/pages/TablePage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Table from '../components/Table'
+
+const TablePage = () => {
+
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: 5 },
+        { name: 'Apple', color: 'bg-red-500', score: 3 },
+        { name: 'Banana', color: 'bg-yellow-500', score: 1 },
+        { name: 'Lime', color: 'bg-green-500', score: 4 },
+    ]
+    const config = [
+        { label:'Fruits'},
+        { label:'Color'},
+        { label:'Score'},
+    ]
+
+    return (
+        <div><Table data={data} config={config}/></div>
+    )
+}
+
+export default TablePage;
