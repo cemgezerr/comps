d9436f41cb4254db2fc357de7d9df2b450aebf0b
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-25 14:11:23 +0300

    table fixed

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-const Table = ({ data , config}) => {
+const Table = ({ data, config, keyFn }) => {
     const renderedHeader = config.map((column) => {
-      return <th key={column.label}>{column.label}</th>
+        return <th key={column.label}>{column.label}</th>
     })
-    const renderedRows = data.map((fruit) => {
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return <td className='p-2' key={column.label}>{column.render(rowData)}</td>
+        })
         return (
-            <tr className='border-b' key={fruit.name}>
-                <td className='p-3'>{fruit.name}</td>
-                <td className='p-3'>
-                    <div className={`p-3 m-2 ${fruit.color}`}>
-                    </div></td>
-                <td className='p-3'>{fruit.score}</td>
+            <tr className='border-b' key={keyFn(rowData)}>
+                {renderedCells}
             </tr>
         )
     })
@@ -19,7 +18,7 @@ const Table = ({ data , config}) => {
         <table className='table-auto border-spacing-2'>
             <thead>
                 <tr className='border-b-2'>
-                 {renderedHeader}
+                    {renderedHeader}
                 </tr>
             </thead>
             <tbody>
