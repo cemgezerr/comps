1dd52a6b60b18eeb312a667a58688959a2d1905e
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-26 12:13:09 +0300

    sorttable setup

--- src/components/SorttableTable.js
+++ src/components/SorttableTable.js
@@ -1,10 +1,82 @@
 import React from 'react'
 import Table from './Table'
+import { useState } from 'react';
+import { GoArrowDown, GoArrowUp } from "react-icons/go";
 
 const SorttableTable = (props) => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
+
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder('asc')
+      setSortBy(label)
+    }
+    else if (sortOrder === 'asc') {
+      setSortOrder('desc')
+      setSortBy(label)
+    }
+    else if (sortOrder === 'desc') {
+      setSortOrder(null)
+      setSortBy(null)
+    }
+  }
+
+  const updateConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column
+    }
+    return {
+      ...column,
+      header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}>
+        <div className='flex items-center'>
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
+        </div>
+      </th>
+    }
+  })
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find(column => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a)
+      const valueB = sortValue(b)
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    })
+  }
   return (
-   <Table {...props}/>
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} data={sortedData} config={updateConfig} />
+    </div>
+
   )
+}
+const getIcons = (label, sortBy, sortOrder) => {
+  if (label !== sortBy) {
+    return <div>
+      <GoArrowUp />
+      <GoArrowDown />
+    </div>
+  } else if (sortOrder === 'asc') {
+    return <div>
+      <GoArrowUp />
+    </div>
+  } else if (sortOrder === 'desc') {
+    return <div>
+      <GoArrowDown />
+    </div>
+  }
 }
 
 export default SorttableTable
