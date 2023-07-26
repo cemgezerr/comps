113869ea65fbd3a0359ba859e77be5718ec93e25
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-26 15:18:45 +0300

    sorttable change hooks

--- src/components/SorttableTable.js
+++ src/components/SorttableTable.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import Table from './Table'
-import { useState } from 'react';
 import { GoArrowDown, GoArrowUp } from "react-icons/go";
+import useSort from '../hooks/use-sort';
 
 const SorttableTable = (props) => {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
+  const {
+    sortOrder,
+    sortBy,
+    sortedData,
+    setSortColumn
+  } = useSort(data,config)
 
-  const handleClick = (label) => {
-    if (sortOrder === null) {
-      setSortOrder('asc')
-      setSortBy(label)
-    }
-    else if (sortOrder === 'asc') {
-      setSortOrder('desc')
-      setSortBy(label)
-    }
-    else if (sortOrder === 'desc') {
-      setSortOrder(null)
-      setSortBy(null)
-    }
-  }
+
 
   const updateConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -29,7 +20,7 @@ const SorttableTable = (props) => {
     }
     return {
       ...column,
-      header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}>
+      header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => setSortColumn(column.label)}>
         <div className='flex items-center'>
           {getIcons(column.label, sortBy, sortOrder)}
           {column.label}
@@ -38,25 +29,9 @@ const SorttableTable = (props) => {
     }
   })
 
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find(column => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a)
-      const valueB = sortValue(b)
 
-      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-      if (typeof valueA === 'string') {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    })
-  }
   return (
     <div>
-      {sortOrder} - {sortBy}
       <Table {...props} data={sortedData} config={updateConfig} />
     </div>
 
